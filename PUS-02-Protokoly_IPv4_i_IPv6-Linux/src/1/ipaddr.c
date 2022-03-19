1bb05ccbee750a5e6054f4dfd76b9415e6542c93
mscieszka <scieszka.marcin@pm.me>
2022-03-19 14:20:15 +0100

    add Makefiles, restructure

--- PUS-02-Protokoly_IPv4_i_IPv6-Linux/src/1/ipaddr.c
+++ PUS-02-Protokoly_IPv4_i_IPv6-Linux/src/1/ipaddr.c
@@ -12,26 +12,27 @@
 #include <stdlib.h>
 #include <errno.h>
 
-char* ipv4_binary_string(uint32_t addr);
+char *ipv4_binary_string(uint32_t addr);
 
-int main(int argc, char** argv) {
-
-    int                     i;
-    int                     retval; /* Wartosc zwracana przez funkcje. */
-    struct sockaddr_in      addr4;  /* Struktura adresowa dla IPv4. */
-    struct sockaddr_in6     addr6;  /* Struktura adresowa dla IPv6. */
+int main(int argc, char **argv)
+{
+    int i;
+    int retval;                /* Wartosc zwracana przez funkcje. */
+    struct sockaddr_in addr4;  /* Struktura adresowa dla IPv4. */
+    struct sockaddr_in6 addr6; /* Struktura adresowa dla IPv6. */
 
     /* Bufory na adresy w postaci czytelnej dla czlowieka (tekstowej): */
-    char                    buff4[INET_ADDRSTRLEN];
-    char                    buff6[INET6_ADDRSTRLEN];
+    char buff4[INET_ADDRSTRLEN];
+    char buff6[INET6_ADDRSTRLEN];
 
     /*
      * Flagi, ktore sluza do oznaczenia, ze konwersja adresu IPv4 lub IPv6
      * nie powiodla sie:
      */
-    int                     invalid4, invalid6;
+    int invalid4, invalid6;
 
-    if (argc != 2) {
+    if (argc != 2)
+    {
         fprintf(stderr, "Invocation: %s <IPv4 OR IPv6 ADDRESS>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
@@ -46,24 +47,24 @@ int main(int argc, char** argv) {
     /******************************************************************************/
     fprintf(
         stdout,
-        "Internet host address to network address structure conversion:\n\n"
-    );
+        "Internet host address to network address structure conversion:\n\n");
 
     /*
      * Funkcja inet_aton() przeprowadza konwersje adresu IPv4 z postaci
      * kropkowo-dziesietnej (pierwszy parametr) do postaci zrozumialej dla maszyny
      * (drugi parametr). Drugim parametrem jest wskaznik na strukture in_addr.
      */
-    if (inet_aton(argv[1], &(addr4.sin_addr)) == 0) {
+    if (inet_aton(argv[1], &(addr4.sin_addr)) == 0)
+    {
         fprintf(stderr, "inet_aton(): invalid IPv4 address!\n");
         invalid4 = 1; /* Blad konwersji adresu IPv4. */
-    } else {
+    }
+    else
+    {
         fprintf(
             stdout, "inet_aton(): %s (binary)\n",
-            ipv4_binary_string(addr4.sin_addr.s_addr)
-        );
+            ipv4_binary_string(addr4.sin_addr.s_addr));
     }
-
 
     /*
      * Funkcja inet_addr() przeprowadza konwersje adresu IPv4 z postaci
@@ -73,14 +74,16 @@ int main(int argc, char** argv) {
      * <netinet/in.h>:
      * typedef uint32_t in_addr_t; *
      */
-    if ((addr4.sin_addr.s_addr = inet_addr(argv[1])) == INADDR_NONE) {
+    if ((addr4.sin_addr.s_addr = inet_addr(argv[1])) == INADDR_NONE)
+    {
         fprintf(stderr, "inet_addr(): invalid IPv4 address!\n");
         invalid4 = 1;
-    } else {
+    }
+    else
+    {
         fprintf(
             stdout, "inet_addr(): %s (binary)\n",
-            ipv4_binary_string(addr4.sin_addr.s_addr)
-        );
+            ipv4_binary_string(addr4.sin_addr.s_addr));
     }
 
     /*
@@ -91,51 +94,63 @@ int main(int argc, char** argv) {
      * Dla IPv4:
      */
     retval = inet_pton(AF_INET, argv[1], &addr4.sin_addr);
-    if (retval == 0) {
+    if (retval == 0)
+    {
         fprintf(stderr, "inet_pton(): invalid IPv4 address!\n");
         invalid4 = 1;
-    } else if (retval == -1) {
+    }
+    else if (retval == -1)
+    {
         perror("inet_pton()");
         exit(EXIT_FAILURE);
-    } else {
+    }
+    else
+    {
         fprintf(
             stdout, "inet_pton(): %s (binary)\n",
-            ipv4_binary_string(addr4.sin_addr.s_addr)
-        );
+            ipv4_binary_string(addr4.sin_addr.s_addr));
     }
 
     /* Dla IPv6: */
     retval = inet_pton(AF_INET6, argv[1], &addr6.sin6_addr);
-    if (retval == 0) {
+    if (retval == 0)
+    {
         fprintf(stderr, "inet_pton(): invalid IPv6 address!\n");
         invalid6 = 1;
-    } else if (retval == -1) {
+    }
+    else if (retval == -1)
+    {
         perror("inet_pton()");
         exit(EXIT_FAILURE);
-    } else {
+    }
+    else
+    {
         /* Wypisanie adresu IPv6 (format heksadecymalny): */
         fprintf(stdout, "inet_pton(): ");
         i = 0;
-        for (;;) {
+        for (;;)
+        {
             /* Wypisanie bajtu (2 cyfr szesnastkowych): */
             fprintf(stdout, "%.2x", addr6.sin6_addr.s6_addr[i]);
 
             ++i;
-            if (i == 16) {
+            if (i == 16)
+            {
                 break;
             }
 
-            if (i%2 == 0) { /* Co 4 cyfry heksadecymalne (2 bajty). */
+            if (i % 2 == 0)
+            { /* Co 4 cyfry heksadecymalne (2 bajty). */
                 fprintf(stdout, ":");
             }
-
         }
         fprintf(stdout, " (hex)\n");
     }
 
     /* Jezeli podany tekst nie moze zostac zaklasyfikowany
      * jako poprawny adres IP: */
-    if (invalid4 & invalid6) {
+    if (invalid4 & invalid6)
+    {
         exit(EXIT_FAILURE);
     }
 
@@ -145,14 +160,14 @@ int main(int argc, char** argv) {
     /******************************************************************************/
     fprintf(
         stdout,
-        "\nNetwork address structure to internet host address conversion:\n\n"
-    );
+        "\nNetwork address structure to internet host address conversion:\n\n");
 
     /*
      * Jezeli podany w argumencie argv[1] string jest adresem IPv4
      * (przeszedl poprawna konwersje z postaci czytelnej dla czlowieka):
      */
-    if (invalid4 != 1) {
+    if (invalid4 != 1)
+    {
 
         /*
          * Funkcja inet_ntoa() przeprowadza konwersje adresu IPv4 z postaci
@@ -172,40 +187,41 @@ int main(int argc, char** argv) {
          */
         fprintf(
             stdout, "inet_ntop(): %s\n",
-            inet_ntop(AF_INET, &(addr4.sin_addr), buff4, INET_ADDRSTRLEN)
-        );
+            inet_ntop(AF_INET, &(addr4.sin_addr), buff4, INET_ADDRSTRLEN));
     }
 
     /*
      * Jezeli podany w argumencie argv[1] string jest adresem IPv6
      * (przeszedl poprawna konwersje z postaci czytelnej dla czlowieka):
      */
-    if (invalid6 != 1) {
+    if (invalid6 != 1)
+    {
         /* Wywolanie funkcji inet_ntop() dla IPv6: */
         fprintf(
             stdout, "inet_ntop(): %s\n",
-            inet_ntop(AF_INET6, &(addr6.sin6_addr), buff6, INET6_ADDRSTRLEN)
-        );
+            inet_ntop(AF_INET6, &(addr6.sin6_addr), buff6, INET6_ADDRSTRLEN));
     }
 
     exit(EXIT_SUCCESS);
 }
 
-
 /*
  * Funkja odpowiedzialna za konwersje adresu IPv4 z postaci zrozumialej dla
  * maszyny do stringu reprezentujacego adres w formie binarnej.
  */
-char* ipv4_binary_string(uint32_t addr) {
+char *ipv4_binary_string(uint32_t addr)
+{
     int i, j;
     static char buff[36];
     const uint32_t pos = 0x80000000;
     addr = ntohl(addr);
 
-    for (i=1, j=0; i < 32; ++i, ++j) {
+    for (i = 1, j = 0; i < 32; ++i, ++j)
+    {
         buff[j] = (pos & addr) ? '1' : '0';
         addr <<= 1;
-        if (i%8 == 0) {
+        if (i % 8 == 0)
+        {
             buff[++j] = '.';
         }
     }
--- PUS-02-Protokoly_IPv4_i_IPv6-Linux/src/2/hostname2ip.c
+++ PUS-02-Protokoly_IPv4_i_IPv6-Linux/src/2/hostname2ip.c
@@ -11,23 +11,22 @@
 #include <sys/types.h>
 #include <netdb.h> /* getaddrinfo() */
 
-
-int main(int argc, char** argv) {
-
+int main(int argc, char **argv)
+{
     /* Struktura zawierajaca wskazowki dla funkcji getaddrinfo(): */
-    struct addrinfo         hints;
+    struct addrinfo hints;
 
     /*
      * Wskaznik na liste zwracana przez getaddrinfo() oraz wskaznik uzywany do
      * poruszania sie po elementach listy:
      */
-    struct addrinfo         *result, *rp;
+    struct addrinfo *result, *rp;
 
     /* Wartosc zwracana przez funkcje: */
-    int                     retval;
+    int retval;
 
     /* Zmienna przechowuje wersje protokolu IP (4 lub 6): */
-    int                     ip_version;
+    int ip_version;
 
     /*
      * Adres IPv4 lub IPv6 w postaci tekstowej. Rozmiar nie powinien przekroczyc
@@ -35,38 +34,43 @@ int main(int argc, char** argv) {
      * (okresla maksymalny rozmiar bufora dla nazwy domenowej lub adresu IP
      * w formie tekstowej.
      */
-    char                    address[NI_MAXHOST];
+    char address[NI_MAXHOST];
 
     /* Rozmiar gniazdowej struktury adresowej: */
-    socklen_t               sockaddr_size;
+    socklen_t sockaddr_size;
 
-    if (argc != 2) {
+    if (argc != 2)
+    {
         fprintf(stderr, "Invocation: %s <HOSTNAME>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
     memset(&hints, 0, sizeof(hints));
     /* Pozwalamy na AF_INET or AF_INET6: */
-    hints.ai_family         =       AF_UNSPEC;
+    hints.ai_family = AF_UNSPEC;
     /* Gniazdo typu SOCK_STREAM (TCP): */
-    hints.ai_socktype       =       SOCK_STREAM;
+    hints.ai_socktype = SOCK_STREAM;
     /* Dowolny protokol: */
-    hints.ai_protocol       =       0;
+    hints.ai_protocol = 0;
 
-    if ((retval = getaddrinfo(argv[1], NULL, &hints, &result)) != 0) {
+    if ((retval = getaddrinfo(argv[1], NULL, &hints, &result)) != 0)
+    {
         fprintf(stderr, "getaddrinfo(): %s\n", gai_strerror(retval));
         exit(EXIT_FAILURE);
     }
 
-
     /* Przechodzimy kolejno przez elementy listy: */
-    for (rp = result; rp != NULL; rp = rp->ai_next) {
+    for (rp = result; rp != NULL; rp = rp->ai_next)
+    {
 
         /* Zapamietujemy wersje protokolu oraz rozmiar struktury adresowej: */
-        if (rp->ai_family == AF_INET) { /* IPv4 */
+        if (rp->ai_family == AF_INET)
+        { /* IPv4 */
             ip_version = 4;
             sockaddr_size = sizeof(struct sockaddr_in);
-        } else { /* IPv6 */
+        }
+        else
+        { /* IPv6 */
             ip_version = 6;
             sockaddr_size = sizeof(struct sockaddr_in6);
         }
@@ -76,14 +80,14 @@ int main(int argc, char** argv) {
          * do postaci tekstowej:
          */
         retval = getnameinfo(
-                     (struct sockaddr *)rp->ai_addr,
-                     sockaddr_size,
-                     address, NI_MAXHOST,
-                     NULL, 0,
-                     NI_NUMERICHOST
-                 );
+            (struct sockaddr *)rp->ai_addr,
+            sockaddr_size,
+            address, NI_MAXHOST,
+            NULL, 0,
+            NI_NUMERICHOST);
 
-        if (retval != 0) {
+        if (retval != 0)
+        {
             fprintf(stderr, "getnameinfo(): %s\n", gai_strerror(retval));
             exit(EXIT_FAILURE);
         }
@@ -92,7 +96,8 @@ int main(int argc, char** argv) {
     }
 
     /* Zwalniamy liste zaalokowana przez funkcje getaddrinfo(): */
-    if (result != NULL) {
+    if (result != NULL)
+    {
         freeaddrinfo(result);
     }
 
